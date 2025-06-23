e17847613a49e00fbb5027ff6e81815d71cc1539
caren580 <chumacaren69@gmail.com>
2025-06-23 23:32:19 +0300

    Added loading spinner and formated using prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import { RingLoader } from "react-spinners";
+import "./App.css";
 
 function App() {
-  const [advice, setAdvice] = useState("")
-  const [error, setError] = useState(null)
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function handleAdviceGiver() {
     try {
-      setError("")
-      const response = await fetch("https://api.adviceslip.com/advice")
+      setError("");
+      setLoading(true);
+      const response = await fetch("https://api.adviceslip.com/advice");
       if (!response.ok) {
-        throw new Error("Failed to fetch advice")
+        throw new Error("Failed to fetch advice");
       }
-      const data = await response.json()
-      setAdvice(data.slip.advice)
+      const data = await response.json();
+      setAdvice(data.slip.advice);
     } catch (e) {
-      console.error(e)
-      setAdvice("")
-      setError("Trouble fetching advice.")
+      console.error(e);
+      setAdvice("");
+      setError("Trouble fetching advice.");
+    } finally {
+      setLoading(false);
     }
   }
 
-
   useEffect(() => {
-    handleAdviceGiver()
-  }, [])
+    handleAdviceGiver();
+  }, []);
 
   return (
     <>
@@ -32,10 +36,13 @@ function App() {
         <p>Wahenga once said...</p>
         {error && <p>{error}</p>}
         <h2>{advice}</h2>
-        <button type="button" onClick={handleAdviceGiver}>Advice me</button>
+        <button type="button" onClick={handleAdviceGiver}>
+          Advice me
+        </button>
+        {loading && <RingLoader color="black" loading={loading} size={30} />}
       </div>
     </>
-  )
+  );
 }
 
 export default App;
